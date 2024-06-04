d15ce085ccf1b6465228472ad8e6f039dead4237
online-marathon <61456363+online-marathon@users.noreply.github.com>
2024-06-04 12:15:51 +0300

    Initial commit

---
+++ task2.js
@@ -0,0 +1,10 @@
+// Простилізуйте елементи як показано на картинці task2.png
+// Застосування стилів запишіть у функції applyStyles
+
+
+function applyStyles() {
+     
+}
+
+
+module.exports = applyStyles;
---
+++ task3.js
@@ -0,0 +1,26 @@
+
+// Напишіть скрипт, який за допомогою засобів DOM створить для порожньої HTML-сторінки таку структуру з тегів і їх атрибутів.
+// 
+//   <main class="mainClass check item">         
+//      <div id="myDiv">
+//          <p>First paragraph</p>           
+//      </div>
+//  </main> 
+// 
+// Цю розмітку потрібно вставити у існуючий html елемент із класом "create_elements"
+
+// Виконання записувати у функцію applyStyles()
+
+
+
+
+
+
+function applyStyles() {
+
+}
+
+
+
+
+module.exports = applyStyles;
